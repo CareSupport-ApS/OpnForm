ecbdffcf74620f7a3772f831eabfff3381ba249b
Jakob-Espersen <espersen@caresupport.dk>
2025-03-04 10:11:01 +0100

    try to force https

--- api/routes/api.php
+++ api/routes/api.php
@@ -29,6 +29,7 @@ use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,6 +41,10 @@ use Illuminate\Support\Facades\Storage;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+if(config('app.env') === 'production') {
+    URL::forceScheme('https');
+}
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
